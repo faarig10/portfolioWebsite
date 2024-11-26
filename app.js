283899a9c221b8223aa3b52875b8d9b9ab195f35
Yash Shukla <shuklayash10.97@gmail.com>
2024-11-26 12:58:24 +0530

    Initial Html css and server files

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require('express');
-
+const { create } = require('express-handlebars');
+const path = require('path');
 const app = express();
 
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set("views", path.join(__dirname, "views"));
+const hbs = create({
+    extname: ".hbs",
+    defaultLayout: false,
+});
+app.engine(".hbs", hbs.engine);
+app.set("view engine", ".hbs");
+
 app.get('/', function(req,res){
-    res.send('Hello World');
+    res.render("home.hbs");
 })
 
 const port = 8080;
